ce8e5ac7dfb240848fc952af2d75b4ec1c5c984f
jultdev <andikaraeng32@gmail.com>
2022-12-27 13:18:47 +0800

    model and controller

---
+++ app/Models/AdminModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AdminModel extends Model
+{
+    protected $table            = 'admin';
+    protected $primaryKey       = 'id_admin';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $allowedFields    = [
+        'username',
+        'fullname',
+        'password',
+        'last_login'
+    ];
+}
